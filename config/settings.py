1866339f893d0cddc48e32d4ef9655a4ba424a83
aa0313aa <9536kklk@gmail.com>
2025-06-20 11:25:00 +0900

    Chore: settings.py 수정 및 배포환경 보안설정 반영

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,7 @@
+# settings.py (수정 완료된 버전)
+
 from pathlib import Path
-import os  # <-- 추가
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,11 +11,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# !!! 중요 !!! 이 아래 라인은 원래 파일에 있던 본인의 실제 SECRET_KEY 값으로 꼭 다시 바꿔주세요!
 SECRET_KEY = 'django-insecure-5*&fhneexe4c*3l8lx_5=_o6_3^+i2jrpu=x+*hq+lve&i=jbd'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
+# 1. 보안을 위해 DEBUG는 False로 변경했습니다.
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+# 2. ALLOWED_HOSTS는 그대로 유지합니다.
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'aa0313.pythonanywhere.com', '현금화.site']
 
 
@@ -25,131 +31,4 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django_summernote',  # Summernote 에디터 추가
-    'posts',  # <-- 이 부분을 추가해주세요!
-    'accounts', # <-- 이 줄을 추가!
-    'products',   # ← 반드시 이 줄이 있어야 합니다!
-]
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-ROOT_URLCONF = 'config.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],  # 또는 os.path.join(BASE_DIR, "templates")
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-WSGI_APPLICATION = 'config.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
-# Password validation
-# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
-
-# Internationalization
-# https://docs.djangoproject.com/en/5.2/topics/i18n/
-
-LANGUAGE_CODE = 'ko-kr'
-
-TIME_ZONE = 'Asia/Seoul'
-
-USE_I18N = True
-
-USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "products",
-]
-STATIC_ROOT = BASE_DIR / "staticfiles"
-
-# 미디어 파일 설정 추가
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-LOGOUT_REDIRECT_URL = '/'
-LOGIN_REDIRECT_URL = '/'
-
-# config/settings.py 파일 맨 아래에 추가
-
-SUMMERNOTE_CONFIG = {
-    'summernote': {
-        'width': '100%',
-        'height': '480',
-        'lang': 'ko-KR',
-        'toolbar': [
-            ['style', ['style']],
-            ['font', ['bold', 'italic', 'underline', 'strikethrough', 'clear']],
-            ['fontname', ['fontname']],
-            ['fontsize', ['fontsize']],
-            ['color', ['color']],
-            ['para', ['ul', 'ol', 'paragraph']],
-            ['height', ['height']],
-            ['table', ['table']],
-            ['insert', ['link', 'picture', 'video']],
-            ['view', ['fullscreen', 'codeview', 'help']],
-        ],
-    },
-    'attachment_upload_to': 'attachments/%Y/%m',
-    'attachment_filesize_limit': 1024 * 1024 * 10,  # 10MB
-}
-
-from django.shortcuts import render
-
-def index(request):
-    return render(request, 'products/index.html')
+    'django
